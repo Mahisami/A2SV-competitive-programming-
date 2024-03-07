fd707db20aa79f5bf66c01d7360bec1da672b46e
Mahisami <88837372+Mahisami@users.noreply.github.com>
2024-03-07 12:18:28 -0500

    Add solution for Sqrt(x)

--- leet-code-solutions/leetcode/sqrtx.py
+++ leet-code-solutions/leetcode/sqrtx.py
@@ -2,18 +2,19 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         l = 1
         r = x
+        if x == 0:
+            return 0
 
-        while True:
-            mid = (l+r) // 2
+        while l <= r:
+            mid = l+(r-l)//2
 
-            if mid ** 2 <= x and (mid+1) ** 2 > x:
+            if mid == (x // mid):
                 return mid
-
-            elif (mid ** 2) > x and (mid + 1) ** 2 > x:
+            elif mid > (x//mid):
                 r = mid - 1
             else:
-                l= mid + 1
-        # return r
+                l = mid + 1
+        return r
 
 
 
